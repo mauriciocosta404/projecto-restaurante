df436646ec4d901a4ebf611a9518eebbe8b17b7a
menezescosta <menezescosta>
2022-06-13 02:47:11 +0100

    header feito

---
+++ script.js
@@ -0,0 +1,35 @@
+const menu=document.querySelector('header .bars');
+let navbar=document.querySelector('.navbar');
+
+menu.addEventListener('click',(event)=>{
+    navbar.classList.toggle('active');
+    }
+);
+
+
+window.onscroll=()=>{
+    navbar.classList.toggle('active');
+}
+
+document.querySelector('.search-icon-container').addEventListener('click',(event)=>{
+    document.querySelector('#search-form').classList.toggle('active');
+});
+
+
+document.querySelector('.close-icon-container').addEventListener('click',(event)=>{
+    document.querySelector('#search-form').classList.remove('active');
+});
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  });
